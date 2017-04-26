08876a67fcd20b2872a87c94f0e9483f25f3471a
Javier Vargas <javier1615@gmail.com>
2017-04-25 21:03:09 -0500

    add-models-for-comments-and-votes-and-polymorphic-associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,7 @@ class Question < ApplicationRecord
 	has_many :answers
 	has_many :comments
 	has_many :votes
+	has_many :comments, as: :commentable
+	has_many :votes, as: :votable
 	belongs_to :user
 end
---
+++ db/migrate/20170426005017_create_votes.rb
@@ -0,0 +1,10 @@
+class CreateVotes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :votes do |t|
+    	t.integer :votable_id
+      	t.string  :votable_type
+      	t.references :user, index: true
+      t.timestamps
+    end
+  end
+end
